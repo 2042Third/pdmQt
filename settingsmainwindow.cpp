6987d24860ef75fc3c7aae60f41b8393057b75d6
mike yang <18604713262@163.com>
2023-02-12 23:33:39 -0500

    Settings, custom tab and dynamic tabs tracking and deleting.

--- settingsmainwindow.cpp
+++ settingsmainwindow.cpp
@@ -1,13 +1,27 @@
 #include "settingsmainwindow.h"
 #include "ui_settingsmainwindow.h"
-
-
+#include "accountwidget.h"
+#include "informationwidget.h"
 
 SettingsMainWindow::SettingsMainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::SettingsMainWindow)
 {
     ui->setupUi(this);
+}
+
+bool SettingsMainWindow::destroyWidget(){
+    QList< QWidget* > children;
+
+    children = ui->gridLayout->findChildren< QWidget* >();
+    for (unsigned int i=0; i<children.count();i++) {
+        if (children.at( i )->objectName() == "widget") {
+            delete children.at(i);
+            return true;
+        }
+    }
+
+    return false;
 }
 
 SettingsMainWindow::~SettingsMainWindow()
@@ -17,6 +31,34 @@ SettingsMainWindow::~SettingsMainWindow()
 
 void SettingsMainWindow::on_settingAccountButton_released()
 {
+    acc_ui = new AccountWidget(ui->centralwidget);
+    acc_ui->setObjectName("widget");
+    if(lastWidget!=nullptr){
+        ui->gridLayout->removeWidget(lastWidget);
+        delete lastWidget;
+    }
+    ui->gridLayout->addWidget(acc_ui,2,1,1,3);
+    lastWidget = acc_ui;
 
+}
+
+
+void SettingsMainWindow::on_widget_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
+
+void SettingsMainWindow::on_settingInfoButton_released()
+{
+    info_ui = new InformationWidget(ui->centralwidget);
+    info_ui -> setObjectName("widget");
+
+    if(lastWidget!=nullptr){
+        ui->gridLayout->removeWidget(lastWidget);
+        delete lastWidget;
+    }
+    ui->gridLayout->addWidget(info_ui,2,1,1,3);
+    lastWidget = info_ui;
 }
 
--- settingsmainwindow.h
+++ settingsmainwindow.h
@@ -14,9 +14,17 @@ class SettingsMainWindow : public QMainWindow
 public:
     explicit SettingsMainWindow(QWidget *parent = nullptr);
     ~SettingsMainWindow();
+    bool destroyWidget();
+    QWidget *lastWidget = nullptr;
+    QWidget * acc_ui;
+    QWidget * info_ui;
 
 private slots:
     void on_settingAccountButton_released();
+
+    void on_widget_customContextMenuRequested(const QPoint &pos);
+
+    void on_settingInfoButton_released();
 
 private:
     Ui::SettingsMainWindow *ui;
