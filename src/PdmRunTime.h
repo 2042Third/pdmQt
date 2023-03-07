ebaaf98da6490c61f71564833b03cd18d30953f2
mike yang <18604713262@163.com>
2023-03-06 23:11:30 -0500

    User config, User information, and debug information local encrypted database ported in.

--- src/PdmRunTime.h
+++ src/PdmRunTime.h
@@ -10,12 +10,22 @@
   class PdmRunTime : public QObject,
       public PDM::Status,
       public PDM::network {
+
   Q_OBJECT
   public:
+
+    PDM::pdm_database * db; // debug
+    PDM::pdm_database * user_conf; // User config
+    PDM::pdm_database * user_data; // User data
     explicit PdmRunTime(QObject *parent = nullptr);
+    ~PdmRunTime();
+    std::string conf_loc;
+    std::string data_loc;
 
+    int signin_action(const std::string&a, NetWriter* wt_in,const char* password,const char* email);
+    int get_user_loc(const std::string &file_names,int conf=1);
   signals:
-
     void log(const QString &message, const QString &color);
+
   };
 #endif // PDMRUNTIME_H
