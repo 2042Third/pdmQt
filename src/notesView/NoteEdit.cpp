f50ae222c735d83629a657964db3bab58ff48dd3
mike yang <18604713262@163.com>
2023-05-08 12:36:35 -0400

    Note edit clean up and overwrite memory on exit

--- src/notesView/NoteEdit.cpp
+++ src/notesView/NoteEdit.cpp
@@ -17,3 +17,33 @@ NoteEdit::NoteEdit(QWidget *parent) :QTextEdit(parent){
 
 }
 
+NoteEdit::~NoteEdit(){
+  clearNoteMsg(m_note);
+  clearEditText();
+}
+
+void NoteEdit::clearNoteMsg(PDM::NoteMsg& noteMsg) {
+  noteMsg.head.assign(noteMsg.head.length(), 0);
+  noteMsg.note_id.assign(noteMsg.note_id.length(), 0);
+  noteMsg.uid = -1;
+  noteMsg.time = -1;
+  noteMsg.update_time = -1;
+  noteMsg.utime.assign(noteMsg.utime.length(), 0);
+  noteMsg.ctime.assign(noteMsg.ctime.length(), 0);
+  noteMsg.key = -1;
+  noteMsg.content.assign(noteMsg.content.length(), 0);
+  noteMsg.email.assign(noteMsg.email.length(), 0);
+  noteMsg.session.assign(noteMsg.session.length(), 0);
+  noteMsg.ntype.assign(noteMsg.ntype.length(), 0);
+  noteMsg.sess.assign(noteMsg.sess.length(), 0);
+  noteMsg.h.assign(noteMsg.h.length(), 0);
+  noteMsg.username.assign(noteMsg.username.length(), 0);
+  noteMsg.status.assign(noteMsg.status.length(), 0);
+  noteMsg.statusInfo.assign(noteMsg.statusInfo.length(), 0);
+  noteMsg.encry.assign(noteMsg.encry.length(), 0);
+  noteMsg.hash.assign(noteMsg.hash.length(), 0);
+}
+
+void NoteEdit::clearEditText() {
+  setPlainText(QString(toPlainText().size(),'0'));
+}
--- src/notesView/NoteEdit.h
+++ src/notesView/NoteEdit.h
@@ -17,20 +17,21 @@ Q_OBJECT
 public:
   explicit NoteEdit(PDM::NoteMsg note, QWidget *parent = nullptr) ;
   explicit NoteEdit( QWidget *parent = nullptr) ;
+  ~NoteEdit() ;
 
-  // Accessor methods to get and set the NoteMsg object
-  PDM::NoteMsg* getNote() {return &m_note;}
-
+  void clearNoteMsg(PDM::NoteMsg& noteMsg) ;
+  PDM::NoteMsg* getNote() {return &m_note;}// Accessor methods to get and set the NoteMsg object
   void setNote(const PDM::NoteMsg &note) {
     m_note = note;
-    // Update the QTextEdit content when the note is changed
-    setPlainText(QString::fromStdString(m_note.content));
+    setPlainText(QString::fromStdString(m_note.content));// Update the QTextEdit content when the note is changed
   }
 
   void setNote(){ setPlainText(m_note.content.c_str());}
 
 private:
   PDM::NoteMsg m_note;
+
+  void clearEditText();
 };
 
 
