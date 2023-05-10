63deee7b20d8f413b1ae93fdc86bf430eee26151
mike yang <18604713262@163.com>
2023-05-10 15:15:59 -0400

    Note list graphics

--- src/notesView/NotesScrollDelegate.h
+++ src/notesView/NotesScrollDelegate.h
@@ -24,29 +24,31 @@ public:
     style->drawPrimitive(QStyle::PE_PanelItemViewItem, &opt, painter, opt.widget);
 
     QIcon icon = QIcon(":/images/icon/file.svg");
-    QPixmap pixmap = icon.pixmap(QSize(16, 16)); // Set your desired size
+    QPixmap pixmap = icon.pixmap(QSize(25, 25)); // Set your desired size
 
     // Draw the icon
     QRect iconRect = QRect(option.rect.topLeft() + QPoint(5, (option.rect.height() - pixmap.height()) / 2), pixmap.size());
     painter->drawPixmap(iconRect, pixmap);
 
     // Adjust the rect for the text
-    QRect textRect = option.rect.adjusted(pixmap.width() + 10, 0, 0, 0); // Adjust the left margin
+    QRect textRect = option.rect.adjusted(pixmap.width() + 18, 0, 0, 0); // Adjust the left margin
 
     auto note = index.data(Qt::DisplayRole).toString();
     auto noteParts = note.split("\n\n");
 
     QFont font = painter->font();
-    font.setPointSize(20);  // Change the size to your preferred value
+    font.setPointSize(14);  // Change the size to your preferred value
     painter->setFont(font);
 
-    painter->drawText(textRect, Qt::AlignLeft, noteParts[0]);
+    painter->drawText(textRect, Qt::AlignLeft | Qt::AlignVCenter, noteParts[0]);
 
     font.setPointSize(10);  // Change the size back or to a different value
-    painter->setFont(font);
 
-    // Align time to the right
-    painter->drawText(textRect, Qt::AlignRight | Qt::AlignBottom, noteParts[1]);
+    painter->setFont(font);
+    painter->setPen(Qt::gray);
+    painter->drawText(textRect, Qt::AlignRight | Qt::AlignVCenter, noteParts[1]);
+    painter->setPen(Qt::black);
+
   }
 
   QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override
