83f8137ee98e8da6c24def5b284cd95813758594
mike yang <18604713262@163.com>
2023-09-11 21:45:19 -0400

    Fix and rewritten account information.

--- src/settings/userinformation.cpp
+++ src/settings/userinformation.cpp
@@ -1,14 +1,14 @@
 #include "userinformation.h"
 #include "ui_userinformation.h"
 #include "handler/pdm_qt_helpers.h"
-#include <QWidget>
-#include <QString>
+#include <QGridLayout>
+#include <QPushButton>
 
 UserInformation::UserInformation(QWidget *parent, PdmRunTime* rtIn  ) :
     QWidget(parent), PdmRuntimeRef(rtIn),
     ui(new Ui::UserInformation)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 
   usernameLabel = new QLabel("Username:");
   emailLabel = new QLabel("Email:");
@@ -20,20 +20,33 @@ UserInformation::UserInformation(QWidget *parent, PdmRunTime* rtIn  ) :
 
   // Set the line edits(QLabel) to be centered and bold, and
   // labels selectable, and labels' hover mouse cursor to I-beam.
-  usernameLineEdit->setAlignment(Qt::AlignCenter); usernameLineEdit->setStyleSheet("font-weight: bold;");
-  usernameLineEdit->setTextInteractionFlags(Qt::TextSelectableByMouse); usernameLineEdit->setCursor(Qt::IBeamCursor);
-  emailLineEdit->setAlignment(Qt::AlignCenter); emailLineEdit->setStyleSheet("font-weight: bold;");
-  emailLineEdit->setTextInteractionFlags(Qt::TextSelectableByMouse); emailLineEdit->setCursor(Qt::IBeamCursor);
-  creationDateLineEdit->setAlignment(Qt::AlignCenter); creationDateLineEdit->setStyleSheet("font-weight: bold;");
-  creationDateLineEdit->setTextInteractionFlags(Qt::TextSelectableByMouse); creationDateLineEdit->setCursor(Qt::IBeamCursor);
+  usernameLineEdit->setAlignment(Qt::AlignCenter);
+  usernameLineEdit->setStyleSheet("font-weight: bold;");
+  usernameLineEdit->setTextInteractionFlags(Qt::TextSelectableByMouse);
+  usernameLineEdit->setCursor(Qt::IBeamCursor);
 
-  layout = new QVBoxLayout;
-  layout->addWidget(usernameLabel);
-  layout->addWidget(usernameLineEdit);
-  layout->addWidget(emailLabel);
-  layout->addWidget(emailLineEdit);
-  layout->addWidget(creationDateLabel);
-  layout->addWidget(creationDateLineEdit);
+  emailLineEdit->setAlignment(Qt::AlignCenter);
+  emailLineEdit->setStyleSheet("font-weight: bold;");
+  emailLineEdit->setTextInteractionFlags(Qt::TextSelectableByMouse);
+  emailLineEdit->setCursor(Qt::IBeamCursor);
+
+  creationDateLineEdit->setAlignment(Qt::AlignCenter);
+  creationDateLineEdit->setStyleSheet("font-weight: bold;");
+  creationDateLineEdit->setTextInteractionFlags(Qt::TextSelectableByMouse);
+  creationDateLineEdit->setCursor(Qt::IBeamCursor);
+
+  QPushButton* signOutButton = new QPushButton("Sign Out and Delete Local Data");
+  // connect the button's signal to some slot here if you want it to perform some action
+  // connect(signOutButton, &QPushButton::clicked, this, &UserInformation::yourFunctionName);
+
+  QGridLayout *layout = new QGridLayout;
+  layout->addWidget(usernameLabel, 0, 0);
+  layout->addWidget(usernameLineEdit, 0, 1);
+  layout->addWidget(emailLabel, 1, 0);
+  layout->addWidget(emailLineEdit, 1, 1);
+  layout->addWidget(creationDateLabel, 2, 0);
+  layout->addWidget(creationDateLineEdit, 2, 1);
+  layout->addWidget(signOutButton, 3, 0, 1, 2);
 
   setLayout(layout);
 }
@@ -41,14 +54,7 @@ UserInformation::UserInformation(QWidget *parent, PdmRunTime* rtIn  ) :
 UserInformation::~UserInformation()
 {
   delete ui;
-
-  delete usernameLabel;
-  delete emailLabel;
-  delete creationDateLabel;
-  delete usernameLineEdit;
-  delete emailLineEdit;
-  delete creationDateLineEdit;
-  delete layout;
+  // Child widgets will be automatically deleted by Qt. So, there's no need to delete them manually.
 }
 
 void UserInformation::displayUserInfo() {
